9a132e79ec3bdd6465df292ee5edc0d9fe9da8a1
gracesessions <98703832+gracesessions@users.noreply.github.com>
2022-04-19 16:57:19 +0100

    synonym display

--- js/wordfinder.js
+++ js/wordfinder.js
@@ -1,39 +1,55 @@
+let answer; // this is a global variable, the result of the pickWord() function. This is bad practice.
+let usedWords = [];
+let allData = {};
+
 // Fetch data from json file and return the words object without the description.
+const startButton = document.querySelector('#start');
+
+const handleButtonClick = (event) => {
+    event.preventDefault();
+    answer = pickWord(allData);
+    console.log(answer);
+    return answer;
+}
+
+startButton.addEventListener('click', handleButtonClick);
 
 const extractWords = (response) => {
     const wordList = response.json();
     return wordList;
 }
-
 const processWords = (wordList) => {
-    const allData = wordList.data;
-    answer = pickWord(allData);
+    allData = wordList.data;
+    console.log(allData);
+    return allData;
 }
 
 fetch('json/words.json')
     .then(extractWords)
     .then(processWords);
 
-let answer; // this is a global variable, the result of the pickWord() function. This is bad practice.
-
 function pickWord(allData) {
 
-    let allWords = (Object.keys(allData));
-    let targetWord = allWords[Math.floor(Math.random() * allWords.length)];
-    console.log(targetWord);
-    let targetSyns = allData[targetWord];
-    targetSyns.forEach(item => {
-    })
-    let html = '';
-    targetSyns.forEach(item => {
-        let htmlSegment = `<div class="datum">
-                          <h2>${item}</h2>
-                         </div>`;
-        html += htmlSegment;
+    let allKeys = (Object.keys(allData));
+    let randomWord = allKeys[Math.floor(Math.random() * allKeys.length)];
+    console.log(randomWord);
+    let synonyms = allData[randomWord];
+    let container = document.querySelector('#container');
+    console.log(synonyms);
+    console.log(typeof(synonyms));
+    synonyms.forEach(item => {
+        const listItem = document.createElement('p');
+        const textNode = document.createTextNode(`${item}`);
+        listItem.appendChild(textNode);
+        container.appendChild(listItem);
+        console.log(item);
     });
-    let container = document.querySelector('.container');
-    container.innerHTML = html;
-    return targetWord;
+
+
+
+
+    // console.log(container);
+    return randomWord;
 }
 
 const submitButton = document.querySelector('.submit-btn');
@@ -47,6 +63,7 @@ const submitFunction = (event) => {
 
     if (answer === userGuess) {
         console.log('Nice');
+
     } else {
         console.log('You suck');
     }
