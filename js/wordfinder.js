213a4de2bf78b55f15626e91b0fdce6c70049843
gracesessions <98703832+gracesessions@users.noreply.github.com>
2022-04-19 12:13:30 +0100

    form and synonyms

--- js/wordfinder.js
+++ js/wordfinder.js
@@ -6,10 +6,32 @@ const extractWords = (response) => {
 }
 
 const processWords = (wordList) => {
-    return wordList.data;
+    const allData = wordList.data;
+    pickWord(allData);
 }
 
-fetch('json/words.json')
+console.log(fetch('json/words.json')
     .then(extractWords)
-    .then(processWords);
+    .then(processWords));
 
+function pickWord(allData) {
+    let allWords = (Object.keys(allData));
+    console.log(typeof (allWords));
+    console.log(allWords);
+    let targetWord = allWords[Math.floor(Math.random() * allWords.length)];
+    console.log(targetWord);
+    let targetSyns = allData[targetWord];
+    console.log(targetSyns);
+    targetSyns.forEach(item => {
+        console.log(item)
+    })
+    let html = '';
+    targetSyns.forEach(item => {
+        let htmlSegment = `<div class="datum">
+                          <h2>${item}</h2>
+                         </div>`;
+        html += htmlSegment;
+    });
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+};
