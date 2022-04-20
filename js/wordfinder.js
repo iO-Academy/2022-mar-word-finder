e00b7229c62c3db073abc6f9b1043bb6ccee662e
gracesessions <98703832+gracesessions@users.noreply.github.com>
2022-04-20 11:47:20 +0100

    hide play gain

--- js/wordfinder.js
+++ js/wordfinder.js
@@ -24,6 +24,7 @@ const startButton = document.querySelector('#start');
 
 const handleButtonClick = (event) => {
     event.preventDefault();
+    playAgain.style.display ='none';
     answer = pickWord(allData);
     console.log(answer);
     return answer;
